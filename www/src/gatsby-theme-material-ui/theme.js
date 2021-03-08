dd86041a960079cec6d06b63e7b69a96f36bcdc2
Aaron R Miller <amiller@watheia.org>
2021-03-08 01:46:12 +0000

    remove existing production site
    Signed-off-by: Aaron R Miller <amiller@watheia.org>

--- www/src/gatsby-theme-material-ui/theme.js
+++
@@ -1,8 +0,0 @@
-import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
-
-// A custom theme for this app
-const theme = createMuiTheme({
-  //
-});
-
-export default responsiveFontSizes(theme);
